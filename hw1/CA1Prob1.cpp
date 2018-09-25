59ef8aeff3467de5757ab395f15992ac9714dd78
teric1024 <32102104+teric1024@users.noreply.github.com>
2018-09-25 21:33:37 +0800

    fix problem1

--- hw1/CA1Prob1.cpp
+++ hw1/CA1Prob1.cpp
@@ -11,17 +11,10 @@
 using namespace std;
 
 //dot vector a and b
-int dotProduct(int a[], int b[])
+int dotProduct(int a[], int b[], int n)
 {
-    int na = 0, nb = 0, ans = 0;
-    na = sizeof(a) / sizeof(a[0]);
-    nb = sizeof(b) / sizeof(b[0]);
-    if(na != nb)
-    {
-        cout << "the lengths of two vectors are not same.";
-        return -1;
-    }
-    for(int i = 0; i < na; i += 1)
+    int ans = 0;
+    for(int i = 0; i < n; i += 1)
     {
         ans += a[i] * b[i];
     }
@@ -32,6 +25,6 @@ int dotProduct(int a[], int b[])
 {
     int a[] = {-1, 0, 2, 15, 7, 6, -4, 8, 21, -13};
     int b[] = {5, 9, -18, 16, 0, 1, -4, 18, -2, 12};
-    cout << dotProduct(a,b);
+    cout << dotProduct(a,b,3);
     return 0;
 }*/
